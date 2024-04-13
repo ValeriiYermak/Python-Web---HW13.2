b22f18721ebe2b5773a5f9a76260d5569c7c7e8d
Val <yermakvaleriy@gmail.com>
2024-04-13 17:00:23 +0300

    10/04/2024-2

--- hw_project/app_auth/urls.py
+++ hw_project/app_auth/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import LoginView, LogoutView, \
-    PasswordResetConfirmView
+    PasswordResetConfirmView, PasswordResetCompleteView, PasswordResetDoneView
 from . import views
 from django.urls import path
 from .forms import LoginForm
@@ -9,26 +9,9 @@ app_name = "app_auth"
 
 urlpatterns = [
     path("signup/", views.RegisterView.as_view(), name="signup"),
-    path("signin/", LoginView.as_view(template_name="app_auth/login.html",
-                                      form_class=LoginForm,
-                                      redirect_authenticated_user=True),
-         name="signin"),
-    path("logout/", LogoutView.as_view(template_name="app_auth/logout.html"),
-         name="logout"),
-    path('reset-password/', auth_views.PasswordResetView.as_view(
-        template_name="app_auth/password_reset.html"),
-         name='password_reset'),
-    path('reset-password/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name="app_auth/password_reset_done.html"),
-         name='password_reset_done'),
-    path('reset-password/confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='app_auth/password_reset_confirm.html',
-             success_url='/app_auth/reset-password/complete/'),
-         name='password_reset_confirm'),
-    path('reset-password/complete/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name="app_auth/password_reset_complete.html"),
-         name='password_reset_complete'),
-
+    path("signin/", LoginView.as_view(template_name="app_auth/login.html", form_class=LoginForm, redirect_authenticated_user=True), name="signin"),
+    path("logout/", LogoutView.as_view(template_name="app_auth/logout.html"), name="logout"), path('reset-password/', views.ResetPasswordView.as_view(), name='password_reset'),
+    path('reset-password/done/', PasswordResetDoneView.as_view(template_name='app_auth/password_reset_done.html'), name='password_reset_done'),
+    path('reset-password/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='app_auth/password_reset_confirm.html', success_url='/app_auth/reset-password/complete/'), name='password_reset_confirm'),
+    path('reset-password/complete/', PasswordResetCompleteView.as_view(template_name='app_auth/password_reset_complete.html'), name='password_reset_complete'),
 ]
--- hw_project/hw_project/urls.py
+++ hw_project/hw_project/urls.py
@@ -19,8 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
-    path("", include("quotes.urls")),
-    path("auth/", include("app_auth.urls")),
     path("admin/", admin.site.urls),
+    path("", include("quotes.urls")),
+    path('app_auth/', include('app_auth.urls')),
 ]
